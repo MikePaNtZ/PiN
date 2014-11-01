9a29499f674cd884ac0f3f5909fe5b21cfae669c
Lakon <tjmartin022@gmail.com>
2014-11-01 02:43:44 -0500

    Moved Hud to its own class
    Created a Hud class that handles all the hud stuff. Makes PlatformerGame
    a little cleaner.

--- Platformer/Platformer/Core/PlatformerGame.cs
+++ Platformer/Platformer/Core/PlatformerGame.cs
@@ -29,16 +29,7 @@ namespace Platformer
         private SpriteBatch spriteBatch;
 
         // Global content.
-        private SpriteFont hudFont;
-
-        private Texture2D introOverlay; //so far this is the just team logo. I don't know where to stick it.
-        private Texture2D winOverlay;
-        private Texture2D loseOverlay;
-        private Texture2D diedOverlay;
-
-        //health bar
-        private Texture2D healthBar;
-        private Texture2D healthTexture;
+        private Hud hud;
 
         // Meta-level game state.
         private int levelIndex = -1;
@@ -73,7 +64,6 @@ namespace Platformer
             Content.RootDirectory = "Content";
 
             maps = new List<Map>();
-
         }
 
         /// <summary>
@@ -85,18 +75,7 @@ namespace Platformer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Load fonts
-            hudFont = Content.Load<SpriteFont>("Fonts/Hud");
-
-            // Load overlay textures
-            introOverlay = Content.Load<Texture2D>("Overlays/Intro");
-            winOverlay = Content.Load<Texture2D>("Overlays/you_win");
-            loseOverlay = Content.Load<Texture2D>("Overlays/you_lose");
-            diedOverlay = Content.Load<Texture2D>("Overlays/you_died");
-
-            //health bar
-            healthBar = Content.Load<Texture2D>("Sprites/Player/healthbar");
-            healthTexture = Content.Load<Texture2D>("Sprites/Player/health");
+            hud = new Hud(Content);
 
             cam = new Camera(spriteBatch.GraphicsDevice.Viewport);
 
@@ -199,105 +178,9 @@ namespace Platformer
 
             level.Draw(gameTime, spriteBatch);
 
-            DrawHud();
+            hud.Draw(spriteBatch, level, WarningTime);
 
             base.Draw(gameTime);
-        }
-
-        private void DrawHud()
-        {
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
-            Rectangle titleSafeArea = GraphicsDevice.Viewport.TitleSafeArea;
-            Vector2 hudLocation = new Vector2(titleSafeArea.X, titleSafeArea.Y);
-            Vector2 center = new Vector2(titleSafeArea.X + titleSafeArea.Width / 2.0f,
-                                         titleSafeArea.Y + titleSafeArea.Height / 2.0f);
-            float hudOpacity = 0.7F;
-
-            // Draw time remaining. Uses modulo division to cause blinking when the
-            // activeHero is running out of time.
-            string timeString = "TIME: " + level.TimeRemaining.Minutes.ToString("00") + ":" + level.TimeRemaining.Seconds.ToString("00");
-            Color timeColor;
-            if (level.TimeRemaining > WarningTime ||
-                level.ReachedExit ||
-                (int)level.TimeRemaining.TotalSeconds % 2 == 0)
-            {
-                timeColor = Color.White * hudOpacity;
-            }
-            else
-            {
-                timeColor = Color.Red * hudOpacity;
-            }
-            DrawShadowedString(hudFont, timeString, hudLocation, timeColor);
-
-            // Draw health
-            if (level.Heroes != null)
-            {
-                float timeHeight = hudFont.MeasureString(timeString).Y;
-                
-                //define the health bar positions
-                Vector2 Hero1HealthBarLocation = new Vector2(hudLocation.X, hudLocation.Y + timeHeight * 1.2f);
-                Vector2 Hero2HealthBarLocation = new Vector2(hudLocation.X, hudLocation.Y + Hero1HealthBarLocation.Y + healthBar.Height + 2); //the 2 is the distance between bars
-                Vector2 Hero3HealthBarLocation = new Vector2(hudLocation.X, hudLocation.Y + Hero2HealthBarLocation.Y + healthBar.Height + 2);
-
-                //hero names
-                string Hero1Name = "Kaeden ";
-                string Hero2Name = "Sammie ";
-                string Hero3Name = "Aidan ";
-
-                //drawing hero names
-                DrawShadowedString(hudFont, Hero1Name, Hero1HealthBarLocation, Color.White * hudOpacity);
-                DrawShadowedString(hudFont, Hero2Name, Hero2HealthBarLocation, Color.White * hudOpacity);
-                DrawShadowedString(hudFont, Hero3Name, Hero3HealthBarLocation, Color.White * hudOpacity);
-
-                //updating healthbar locations x so they are next to the names and at an equal x
-                Hero1HealthBarLocation.X = hudFont.MeasureString(Hero1Name).X + Hero1HealthBarLocation.X;
-                Hero2HealthBarLocation.X = Hero1HealthBarLocation.X;
-                Hero3HealthBarLocation.X = Hero1HealthBarLocation.X;
-
-                //draw health bars; The +1s are to compensate for the little offset of the green bar on the background
-                spriteBatch.Draw(healthBar, Hero1HealthBarLocation, Color.White * hudOpacity);
-                spriteBatch.Draw(healthTexture, new Rectangle((int)Hero1HealthBarLocation.X + 1, (int)Hero1HealthBarLocation.Y + 1, level.Heroes[0].Health * 2 - 2, 30), Color.White * hudOpacity);
-
-                spriteBatch.Draw(healthBar, Hero2HealthBarLocation, Color.White * hudOpacity);
-                spriteBatch.Draw(healthTexture, new Rectangle((int)Hero2HealthBarLocation.X + 1, (int)Hero2HealthBarLocation.Y + 1, level.Heroes[1].Health * 2 - 2, 30), Color.White * hudOpacity);
-
-                spriteBatch.Draw(healthBar, Hero3HealthBarLocation, Color.White * hudOpacity);
-                spriteBatch.Draw(healthTexture, new Rectangle((int)Hero3HealthBarLocation.X + 1, (int)Hero3HealthBarLocation.Y + 1, level.Heroes[2].Health * 2 - 2, 30), Color.White * hudOpacity);
-            }
-           
-            // Determine the status overlay message to show.
-            Texture2D status = null;
-            if (level.TimeRemaining == TimeSpan.Zero)
-            {
-                if (level.ReachedExit)
-                {
-                    status = winOverlay;
-                }
-                else
-                {
-                    
-                    status = loseOverlay;
-                }
-            }
-            else if (!level.ActiveHero.IsAlive)
-            {
-                status = diedOverlay;
-            }
-
-            if (status != null)
-            {
-                // Draw status message.
-                Vector2 statusSize = new Vector2(status.Width, status.Height);
-                spriteBatch.Draw(status, center - statusSize / 2, Color.White);
-            }
-
-            spriteBatch.End();
-        }
-
-        private void DrawShadowedString(SpriteFont font, string value, Vector2 position, Color color)
-        {
-            spriteBatch.DrawString(font, value, position + new Vector2(1.0f, 1.0f), Color.Black);
-            spriteBatch.DrawString(font, value, position, color);
         }
     }
 }
