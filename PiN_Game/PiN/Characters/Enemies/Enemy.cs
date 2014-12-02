8bec3318c74328453b90f691c33c88b97900e015
Lakon <tjmartin022@gmail.com>
2014-12-02 03:18:49 -0600

    Made Kamikaze faster
    When enemies are kamikazing they are fast!

--- PiN_Game/PiN/Characters/Enemies/Enemy.cs
+++ PiN_Game/PiN/Characters/Enemies/Enemy.cs
@@ -63,6 +63,8 @@ namespace PiN
             get { return 0.3F; }
         }
 
+        public bool IsKamikaze { get { return ((EnemyStateMachine)stateMachine).BehaviorState is KamikazeState; } }
+
         /// <summary>
         /// How long to shoot
         /// </summary>
